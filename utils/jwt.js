02cf5c0e17bca2abff0069d909b1272b8e210e0d
exelimpichment <oldestspy@gmail.com>
2023-04-12 17:32:06 +0200

    preparation for using as trsting backend for CORS

--- utils/jwt.js
+++ utils/jwt.js
@@ -16,7 +16,7 @@ const attachCookiesToResponse = ({ res, user }) => {
   res.cookie('token', token, {
     // secure: process.env.NODE_ENV === 'production',
     // secure: true,
-    // domain: '.vercel.app',
+    domain: '.onrender.com',
     path: '/',
     maxAge: 24 * 60 * 60 * 1000 * 365,
     signed: true, // <= has to be checked more details what *signed* mean
